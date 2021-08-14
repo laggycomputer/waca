bb426b268c3df0e4f7810060201a84b81af1c34d
Dante Dam <laggycomputer@yahoo.com>
2021-08-14 09:46:17 -0700

    add verbose flag

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const tmp = require("tmp")
 
 const config = require("./config")
 const port = !isNaN(Number(process.argv[2])) ? Number(process.argv[2]) : config.port
+app.locals.is_verbose = Boolean(config.verbose)
 const { version } = require("./package.json")
 
 app.locals.arduino_invocation = config.arduino_invocation
@@ -18,9 +19,14 @@ app.use(function (req, res, next) {
 
 app.set("trust proxy", 1)
 
-app.get("/version", (req, res) => res.json({ version, program: "waca" }))
+app.get("/version", (req, res) => {
+    if (req.app.is_verbose) console.log("info: responding to GET /version")
+    res.json({ version, program: "waca" })
+})
 
 app.get("/boards", (req, res) => {
+    if (req.app.is_verbose) console.log("info: responding to GET /boards")
+
     exec(req.app.locals.arduino_invocation + " board listall --format json", (error, stdout, stderr) => {
         if (error) {
             res.status(500).json({ error: "arduino-cli did not exit properly", stderr: stderr })
@@ -31,6 +37,8 @@ app.get("/boards", (req, res) => {
 })
 
 app.get("/libraries", (req, res) => {
+    if (req.app.is_verbose) console.log("info: responding to GET /libraries")
+    
     exec(req.app.locals.arduino_invocation + " lib list --format json", (error, stdout, stderr) => {
         if (error) {
             res.status(500).json({ error: "arduino-cli did not exit properly", stderr: stderr })
@@ -49,6 +57,8 @@ app.get("/libraries", (req, res) => {
 })
 
 app.post("/compile", express.json(), (req, res) => {
+    if (req.app.is_verbose) console.log("info: responding to POST /compile")
+
     const use_verbose = req.body.verbose === "true"
     const board_fqbn = typeof (req.body.board) === "string" ? req.body.board : "arduino:avr:uno"
     const sketch = req.body.sketch
@@ -58,18 +68,20 @@ app.post("/compile", express.json(), (req, res) => {
     if (/#\s*include\s*"\.*\/.*"/.test(sketch)) {
         res.status(400).send(
             "relative quote imports are not allowed, omit ./ in front of quote import directives\n"
-             + "for example, #include \"./foo.h\" should be #include \"foo.h\"")
+            + "for example, #include \"./foo.h\" should be #include \"foo.h\"")
     }
     const dir_obj = tmp.dirSync({ prefix: "waca-sketch-", unsafeCleanup: true })
     const cleanup = dir_obj.removeCallback
     try {
         if (dir_obj.err) throw dir_obj.err
-        res.send(dir_obj.name)
+        if (req.app.is_verbose) console.log("info: creating temp dir " + dir_obj.name)
     } catch (err) {
         res.status(500).send("failed to allocate temporary sketch folder")
+        console.warn("warn: failed to create a temp dir. this is not normal.")
     }
     finally {
         cleanup()
+        if (req.app.is_verbose) console.log("info: cleaned up temp dir " + dir_obj.name)
     }
 })
 
--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
     "arduino_invocation": "arduino-cli",
-    "port": 4924
+    "port": 4924,
+    "verbose": true
 }

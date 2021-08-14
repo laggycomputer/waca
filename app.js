dabce623f01807d1d276e18556f2fdd0dee3cb89
Dante Dam <laggycomputer@yahoo.com>
2021-08-14 00:12:18 -0700

    add boards route

--- app.js
+++ app.js
@@ -19,6 +19,16 @@ app.set("trust proxy", 1)
 
 app.get("/version", (req, res) => res.json({ version, program: "waca" }))
 
+app.get("/boards", (req, res) => {
+    exec(req.app.locals.arduino_invocation + " board listall --format json", (error, stdout, stderr) => {
+        if (error) {
+            res.status(500).json({error: "arduino-cli did not exit properly", stderr: stderr})
+            return
+        }
+        console.log(stdout)
+        res.json(JSON.parse(stdout))
+    })
+})
 
 exec(config.arduino_invocation + " version", (error) => {
     if (error) {

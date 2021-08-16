949ce93134e4f28569df3f3236558bd213200f93
Dante Dam <laggycomputer@yahoo.com>
2021-08-15 19:46:45 -0700

    fix: allow all the headers

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.locals.arduino_invocation = config.arduino_invocation
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "X-Requested-With")
+    res.header("Access-Control-Allow-Headers", "*")
     next()
 })
 
